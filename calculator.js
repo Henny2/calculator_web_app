83692d69400d93b8e4497d862b63d61df3d17932
Henny2 <henrieke.baunack@web.de>
2022-12-18 16:29:55 -0800

    fixed some issues

--- calculator.js
+++ calculator.js
@@ -1,4 +1,5 @@
 console.log(document)
+// adding the event Listeners for the differnt buttons
 const table_rows = document.getElementById('calculator').rows
 for (let i = 0; i < table_rows.length; i++) {
     for (let j = 0; j < table_rows[i].cells.length; j++) {
@@ -9,33 +10,72 @@ for (let i = 0; i < table_rows.length; i++) {
     }
 }
 let current_operator = ''
-
+let clicked_val = 0
+let calculated_val = 0
 // IDEA: setting one variable for the current_val and one variable for the running sum/diff etc
 
 
-
-
-console.log('end')
+// helper function
+// doing the calculation upon clicking a button 
 function calculate(operator){
-    // current_val = +document.getElementById('calculation').innerText
-    // console.log(current_val) 
-    console.log(document.getElementById(operator).className)
     if (document.getElementById(operator).className === 'operators') {
         // operators 
         console.log('operators clicked')
-        current_operator = operator
+        if (operator === 'C'){
+            calculated_val = 0
+            clicked_val = 0
+            document.getElementById('calculation').innerText = 0
+            current_operator = ""
+
+        }
+        else if(operator ==='='){
+            document.getElementById('calculation').innerText = calculated_val
+        }
+        else {
+            current_operator = operator
+        }
+
     }
     else {
         // numbers
         // display the clicked number 
         console.log('clicked number')
-        if (operator === ""){
+        clicked_val = +operator
+        if (current_operator === ""){
             document.getElementById('calculation').innerText = +operator
+            // clicked_val = +operator
+            calculated_val = clicked_val
         
         }
+        else if (current_operator === "+"){
+            calculated_val = calculated_val + clicked_val
+            current_operator = ''
+        }
+        else if (current_operator === "-"){
+            calculated_val = calculated_val - clicked_val
+            current_operator = ''
+        }
+        else if (current_operator === "*"){
+            calculated_val = calculated_val * clicked_val
+            current_operator = ''
+        }
+        else if (current_operator === "÷"){
+            calculated_val = calculated_val / clicked_val
+            current_operator = ''
+        }
+        else if (current_operator === "="){
+            document.getElementById('calculation').innerText = +calculated_val
+            current_operator = ''
+        }
+        
         document.getElementById('calculation').innerText = +operator
+        clicked_val = +operator
         current_operator = ""
     }
-    console.log(operator)
+    console.log('operator ' + operator)
+    console.log('clicked_val ' + clicked_val.toString())
+    console.log('current_operator ' + current_operator)
+    console.log('calculated value ' + calculated_val.toString())
+
 
 }
